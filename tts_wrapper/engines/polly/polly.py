ff96b3158a683c48b079d587cee947d5bb8d9896
will wade <willwade@gmail.com>
2024-05-07 14:40:41 +0100

    basic working with polly

--- tts_wrapper/engines/polly/polly.py
+++ tts_wrapper/engines/polly/polly.py
@@ -11,13 +11,10 @@ class PollyTTS(AbstractTTS):
     def supported_formats(cls) -> List[FileFormat]:
         return ["wav", "mp3"]
 
-    def __init__(
-        self,
-        client: PollyClient,
-        voice: Optional[str] = None,
-    ) -> None:
+    def __init__(self, client: PollyClient, lang: Optional[str] = None, voice: Optional[str] = None):
+        super().__init__()  # This is crucial
         self._client = client
-        self._voice = voice or "Joanna"
+        self.set_voice(voice or "Joanna", lang or "en-US")
 
     def synth_to_bytes(self, text: Any, format: Optional[FileFormat] = "wav") -> bytes:
         if format not in self.supported_formats():
@@ -31,3 +28,13 @@ class PollyTTS(AbstractTTS):
     def get_voices(self) -> List[Dict[str, Any]]:
         """Retrieves a list of available voices from the Polly service."""
         return self._client.get_voices()
+                
+    def set_voice(self, voice_id: str, lang_id: str):
+        """
+        Sets the voice for the TTS engine and updates the SSML configuration accordingly.
+
+        @param voice_id: The ID of the voice to be used for synthesis.
+        """
+        super().set_voice(voice_id)  # Optionally manage voice at the AbstractTTS level if needed
+        self._voice = voice_id
+        self._lang = lang_id
