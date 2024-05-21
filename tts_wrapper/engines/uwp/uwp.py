25cbc2f7c1dcf387028d07c4f7447f228866e52c
will wade <willwade@gmail.com>
2024-05-22 02:53:37 +0300

    Update uwp.py

--- tts_wrapper/engines/uwp/uwp.py
+++ tts_wrapper/engines/uwp/uwp.py
@@ -2,6 +2,7 @@ from typing import Any, List
 from ...exceptions import UnsupportedFileFormat
 from ...tts import AbstractTTS, FileFormat
 from .client import UWPClient
+from .ssml import UWPSSML
 
 class UWPTTS(AbstractTTS):
     @classmethod
@@ -14,7 +15,8 @@ class UWPTTS(AbstractTTS):
     def synth_to_bytes(self, text: Any, format: FileFormat) -> bytes:
         if format not in self.supported_formats():
             raise UnsupportedFileFormat(format, self.__class__.__name__)
-        return self._client.synth(str(text))
+        ssml = UWPSsml(str(text)).to_ssml()
+        return self._client.synth(ssml)
 
     def get_available_voices(self) -> List[str]:
         return self._client.get_voices()
