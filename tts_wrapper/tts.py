e9888916e132ea76d078210bd8c32eba3f8e74c3
will wade <willwade@gmail.com>
2024-05-22 19:11:16 +0300

    Adding in more event handlers and mirroring pyttsx3

--- tts_wrapper/tts.py
+++ tts_wrapper/tts.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, List, Literal, Optional, Union, Dict
+from typing import Any, List, Literal, Optional, Union, Dict, Callable
 import pyaudio
 import threading
 from threading import Event
@@ -23,10 +23,12 @@ class AbstractTTS(ABC):
         self.position = 0  # Position in the byte stream
         self.timings = []
         self.timers = []
-        self._volume = None
-        self._pitch = None
-        self._pitch_contour = None
-
+        self.properties = {}
+        self.callbacks = {
+            'started-utterance': None,
+            'finished-utterance': None,
+            'started-word': None
+        }
 
     @abstractmethod
     def get_voices(self) -> List[Dict[str, Any]]:
@@ -34,16 +36,9 @@ class AbstractTTS(ABC):
         pass
 
     def set_voice(self, voice_id: str, lang: str = "en-US"):
-        """
-        Sets the voice by its ID and language for synthesis.
-
-        @param voice_id: The ID of the voice to be used for synthesis.
-        @param lang: The language code to be used.
-        """
         self.voice_id = voice_id
         self.lang = lang
 
-        
     @classmethod
     @abstractmethod
     def supported_formats(cls) -> List[FileFormat]:
@@ -52,64 +47,31 @@ class AbstractTTS(ABC):
 
     @abstractmethod
     def synth_to_bytes(self, text: Any, format: Optional[FileFormat] = "wav") -> bytes:
-
-        """
-        Transforms written text to audio bytes on supported formats.
-
-        @param text: Text to be transformed into audio bytes
-        @param format: File format to be used when transforming to audio bytes, defaults to 'wav'
-        @returns: audio bytes created
-        @raises UnsupportedFileFormat: if file format is not supported
-        """
+        """Transforms written text to audio bytes on supported formats."""
         pass
 
     def synth_to_file(self, text: Any, filename: str, format: Optional[FileFormat] = None) -> None:
-        """
-        Transforms written text to an audio file and saves on disk.
-
-        @param text: Text to be transformed to audio file
-        @param filename: Name of the file to be saved on disk
-        @param format: File format to be used when transforming to audio file. Defaults to None.
-        """
         audio_content = self.synth_to_bytes(text, format=format or "wav")
         with open(filename, "wb") as file:
             file.write(audio_content)
 
     def synth(self, text: str, filename: str, format: Optional[FileFormat] = "wav"):
-        """
-        Synthesizes text to speech and directly saves it to a file. Alias
-        """
-        self.synth_to_file(text,filename,format)
+        self.synth_to_file(text, filename, format)
 
     def speak(self, text: Any, format: Optional[FileFormat] = "wav") -> bytes:
-        """
-        Directly plays audio data without using streaming.
-        """
         try:
-            audio_bytes = self.synth_to_bytes(text,format)
-            #lets fade in
+            audio_bytes = self.synth_to_bytes(text, format)
             audio_bytes = self.apply_fade_in(audio_bytes)
-            # Initialize PyAudio and open a stream
             p = pyaudio.PyAudio()
             stream = p.open(format=pyaudio.paInt16, channels=1, rate=self.audio_rate, output=True)
-            # Play the entire byte array
             stream.write(audio_bytes)
-            # Cleanup
             stream.stop_stream()
             stream.close()
             p.terminate()
         except Exception as e:
-            logging(f"Error playing audio: {e}")
-            
+            logging.error(f"Error playing audio: {e}")
             
     def setup_stream(self, format=pyaudio.paInt16, channels=1):
-        """
-        Configures and opens an audio stream with specified format settings.
-        
-        @param format: The sample format (default is 16-bit PCM).
-        @param channels: Number of audio channels (default is 1 for mono).
-        @param rate: Sampling rate in Hz (default is 22050 Hz).
-        """
         try:
             if self.p is None:
                 self.p = pyaudio.PyAudio()
@@ -123,36 +85,22 @@ class AbstractTTS(ABC):
                                       output=True,
                                       stream_callback=self.callback)
         except Exception as e:
-            logging(f"Failed to setup audio stream: {e}")
+            logging.error(f"Failed to setup audio stream: {e}")
             raise
 
-
-
     def callback(self, in_data, frame_count, time_info, status):
-        """
-        Callback function for PyAudio stream to handle audio playback.
-
-        @param in_data: Input data for the stream, not used in output-only mode.
-        @param frame_count: Number of frames per buffer.
-        @param time_info: Dictionary with timing information.
-        @param status: Status flags.
-        @return: A tuple containing the chunk of data to be played and a flag indicating whether to continue.
-        """
         if self.playing:
-            end_position = self.position + frame_count * 2  # *2 because each frame for 16-bit audio is 2 bytes
+            end_position = self.position + frame_count * 2
             data = self.audio_bytes[self.position:end_position]
             self.position = end_position
             if self.position >= len(self.audio_bytes):
-                return (data, pyaudio.paComplete)  # Signal end of buffer
+                self._trigger_callback('finished-utterance', 'utterance', True)
+                return (data, pyaudio.paComplete)
             return (data, pyaudio.paContinue)
         else:
             return (None, pyaudio.paContinue)
-    
 
     def speak_streamed(self, text: Any, format: Optional[FileFormat] = "wav"):
-        """
-        Starts playback of audio data.
-        """
         try:
             audio_bytes = self.synth_to_bytes(text, format)
         except Exception as e:
@@ -160,41 +108,30 @@ class AbstractTTS(ABC):
         self.audio_bytes = self.apply_fade_in(audio_bytes)
         self.position = 0
         self.playing.set()
+        self._trigger_callback('started-utterance', 'utterance')
         if not self.stream:
             self.setup_stream()
         try:
-            # Use a thread to handle playback
             self.play_thread = threading.Thread(target=self._start_stream)
             self.play_thread.start()
         except Exception as e:
-            logging(f"Failed to play audio: {e}")
-            raise  # Correct placement of raise within the except block
+            logging.error(f"Failed to play audio: {e}")
+            raise
 
     def apply_fade_in(self, audio_bytes, fade_duration_ms=50, sample_rate=22050):
-        """
-        Applies a fade-in effect to the start of the audio bytes.
-        `fade_duration_ms` in milliseconds, `sample_rate` in Hz.
-        """
         num_fade_samples = int(fade_duration_ms * sample_rate / 1000)
         fade_in = [i / num_fade_samples for i in range(num_fade_samples)]
     
-        # Convert byte data to a list of samples (16-bit signed integers)
         audio_samples = list(int.from_bytes(audio_bytes[2 * i:2 * i + 2], 'little', signed=True)
                              for i in range(len(audio_bytes) // 2))
     
-        # Apply fade-in by multiplying samples by corresponding fade-in factor
         for i in range(min(len(audio_samples), num_fade_samples)):
             audio_samples[i] = int(audio_samples[i] * fade_in[i])
     
-        # Convert samples back to byte data
         faded_audio_bytes = b''.join((sample.to_bytes(2, 'little', signed=True) for sample in audio_samples))
         return faded_audio_bytes
-
         
     def _start_stream(self):
-        """
-        Starts the stream and waits for it to finish in a thread.
-        """
         if self.stream:
             self.stream.start_stream()
         while self.stream.is_active() and self.playing.is_set():
@@ -205,24 +142,19 @@ class AbstractTTS(ABC):
             self.stream = None
 
     def pause_audio(self):
-        """Pauses the audio playback."""
         self.playing.clear()
 
     def resume_audio(self):
-        """Resumes the paused audio playback."""
-        self.playing.set()  # Set the playing event to resume
+        self.playing.set()
         if not self.stream:
             self.setup_stream()
         if self.stream and not self.stream.is_active():
             self.stream.start_stream()
 
     def stop_audio(self):
-        """
-        Explicitly stops the audio playback and ensures all resources are released.
-        """
-        self.playing.clear()  # Signal to stop playback
+        self.playing.clear()
         if self.play_thread and self.play_thread.is_alive():
-            self.play_thread.join()  # Wait for the playback thread to finish
+            self.play_thread.join()
         if self.stream:
             self.stream.stop_stream()
             self.stream.close()
@@ -232,28 +164,20 @@ class AbstractTTS(ABC):
         self.timers.clear()
 
     def set_timings(self, timing_data):
-        """
-        Sets the timing data for triggering callbacks during audio playback.
-
-        @param timing_data: List of timing information for words or sounds in the audio.
-        Note it should be timing, worded pairs where timing is a float in seconds and word is a string.
-        """
         self.timings = timing_data
 
     def on_word_callback(self, word):
-        """
-        Callback function that is called when a word is spoken.
-
-        @param word: The word that was spoken.
-        """        
         print(f"Word spoken: {word}")
 
-    def start_playback_with_callbacks(self, ssml_text: bytes, callback=None):
-        """
-        Plays back audio with callbacks triggered based on predefined timings.
+    def connect(self, event_name: str, callback: Callable):
+        if event_name in self.callbacks:
+            self.callbacks[event_name] = callback
 
-        @param audio_data: Byte array containing audio data.
-        """ 
+    def _trigger_callback(self, event_name: str, *args):
+        if event_name in self.callbacks and self.callbacks[event_name] is not None:
+            self.callbacks[event_name](*args)
+
+    def start_playback_with_callbacks(self, ssml_text: bytes, callback=None):
         if callback is None:
             callback = self.on_word_callback
 
@@ -275,47 +199,25 @@ class AbstractTTS(ABC):
             if self.p:
                 self.p.terminate()
         except Exception as e:
-            logging(f"Failed to clean up audio resources: {e}")
+            logging.error(f"Failed to clean up audio resources: {e}")
         finally:
             self.stream = None
             self.p = None
-            
 
     def __del__(self):
-        """Cleans up resources upon deletion of the instance."""
-        # Safely check and close the stream
         self.finish()
     
-    @property
-    def rate(self):
-        return self._rate
+    def get_property(self, property_name):
+        return self.properties.get(property_name, None)
 
-    @rate.setter
-    def rate(self, value):
-        self._rate = value
+    def set_property(self, property_name, value):
+        self.properties[property_name] = value
 
-    @property
-    def volume(self):
-        return self._volume
-
-    @volume.setter
-    def volume(self, value):
-        self._volume = value
-
-    @property
-    def pitch(self):
-        return self._pitch
-
-    @pitch.setter
-    def pitch(self, value):
-        self._pitch = value
-
-    @property
-    def pitch_contour(self):
-        return self._pitch_contour
-
-    @pitch_contour.setter
-    def pitch_contour(self, value):
-        self._pitch_contour = value
-
-
+        # Custom handling for specific properties if needed
+        if property_name == "rate":
+            self.audio_rate = value
+        elif property_name == "volume":
+            self._volume = value
+        elif property_name == "pitch":
+            self._pitch = value
+        # Add more custom handling if needed
