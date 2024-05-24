deec6ab226c733bd0c4073242f29fc5a8c3fba48
will wade <willwade@gmail.com>
2024-05-24 08:02:06 +0300

    Wit to WitAi

--- tts_wrapper/engines/witai/ssml.py
+++ tts_wrapper/engines/witai/ssml.py
@@ -1,22 +1,22 @@
 from . import BaseSSMLRoot, SSMLNode, Child
 
-class WitSSML(BaseSSMLRoot):
-    def break_(self, time: str) -> "WitSSML":
+class WitAiSSML(BaseSSMLRoot):
+    def break_(self, time: str) -> "WitAiSSML":
         self.add(SSMLNode("break", attrs={"time": time}))
         return self
 
-    def say_as(self, text: str, interpret_as: str, format: str = None) -> "WitSSML":
+    def say_as(self, text: str, interpret_as: str, format: str = None) -> "WitAiSSML":
         attrs = {"interpret-as": interpret_as}
         if format:
             attrs["format"] = format
         self.add(SSMLNode("say-as", attrs=attrs, children=[text]))
         return self
 
-    def emphasis(self, text: str, level: str = "moderate") -> "WitSSML":
+    def emphasis(self, text: str, level: str = "moderate") -> "WitAiSSML":
         self.add(SSMLNode("emphasis", attrs={"level": level}, children=[text]))
         return self
 
-    def prosody(self, text: str, rate: str = None, pitch: str = None, volume: str = None) -> "WitSSML":
+    def prosody(self, text: str, rate: str = None, pitch: str = None, volume: str = None) -> "WitAiSSML":
         attrs = {}
         if rate:
             attrs["rate"] = rate
@@ -27,15 +27,15 @@ class WitSSML(BaseSSMLRoot):
         self.add(SSMLNode("prosody", attrs=attrs, children=[text]))
         return self
 
-    def phoneme(self, text: str, ph: str, alphabet: str) -> "WitSSML":
+    def phoneme(self, text: str, ph: str, alphabet: str) -> "WitAiSSML":
         self.add(SSMLNode("phoneme", attrs={"ph": ph, "alphabet": alphabet}, children=[text]))
         return self
 
-    def voice(self, text: str, style: str) -> "WitSSML":
+    def voice(self, text: str, style: str) -> "WitAiSSML":
         self.add(SSMLNode("voice", attrs={"style": style}, children=[text]))
         return self
 
-    def sfx(self, text: str, character: str = None, environment: str = None) -> "WitSSML":
+    def sfx(self, text: str, character: str = None, environment: str = None) -> "WitAiSSML":
         attrs = {}
         if character:
             attrs["character"] = character
