69620cc5271b4fddccbb32f7da274fb7c31d2632
will wade <willwade@gmail.com>
2024-06-01 15:21:31 +0100

    matches witai ssml but not great

--- tts_wrapper/engines/microsoft/ssml.py
+++ tts_wrapper/engines/microsoft/ssml.py
@@ -26,3 +26,28 @@ class MicrosoftSSML(BaseSSMLRoot):
         self._inner.update_attributes({"name": new_voice})
         self._root.update_attributes({"xml:lang": new_lang})
 
+    def break_(self, time: str) -> "MicrosoftSSML":
+        self._inner.add(SSMLNode("break", attrs={"time": time}))
+        return self
+
+    def say_as(self, text: str, interpret_as: str, format: str = None) -> "MicrosoftSSML":
+        attrs = {"interpret-as": interpret_as}
+        if format:
+            attrs["format"] = format
+        self._inner.add(SSMLNode("say-as", attrs=attrs, children=[text]))
+        return self
+
+    def emphasis(self, text: str, level: str = "moderate") -> "MicrosoftSSML":
+        self._inner.add(SSMLNode("emphasis", attrs={"level": level}, children=[text]))
+        return self
+
+    def prosody(self, text: str, rate: str = None, pitch: str = None, volume: str = None) -> "MicrosoftSSML":
+        attrs = {}
+        if rate:
+            attrs["rate"] = rate
+        if pitch:
+            attrs["pitch"] = pitch
+        if volume:
+            attrs["volume"] = volume
+        self._inner.add(SSMLNode("prosody", attrs=attrs, children=[text]))
+        return self
