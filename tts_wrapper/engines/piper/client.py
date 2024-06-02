09e10715fa602becbc78e7d0e91be92e35609ff7
will wade <willwade@gmail.com>
2024-06-02 22:09:14 +0100

    getting there with piper. failing to load the client

--- tts_wrapper/engines/piper/client.py
+++ tts_wrapper/engines/piper/client.py
@@ -4,14 +4,17 @@ from typing import Optional, Tuple, Dict, List, Any
 from ...engines.utils import process_wav
 from ...exceptions import ModuleNotInstalled
 import json
+from pathlib import Path
+import os
 
 try:
     print("Importing piper_tts")
-    import piper_tts
-    from piper_tts import PiperVoice, get_voices, VoiceNotFoundError
+    from piper.voice import PiperVoice
+    from piper.download import get_voices, ensure_voice_exists, find_voice, VoiceNotFoundError
+    piper_tts = True  # type: ignore
     print("Imported piper_tts successfully")
 except ImportError as e:
-    piper_tts = None  # type: ignore
+    piper_tts = False  # type: ignore
     print("Piper TTS not installed:", e)
 
 
@@ -24,17 +27,60 @@ FORMATS = {
 
 logger = logging.getLogger(__name__)
 
+
+
 class PiperClient:
     def __init__(
         self,
-        model_path: Optional[str] = "en_US-lessac-medium.onnx",
+        model_path: Optional[str] = "en_US-lessac-medium",
         config_path: Optional[str] = None,
-        use_cuda: Optional[bool] = False
+        use_cuda: Optional[bool] = False,
+        download_dir: Optional[str] = None
     ) -> None:
-        if piper_tts is None:
+        if piper_tts is False:
             raise ModuleNotInstalled("piper-tts")
-        access_key = credentials
-        self._client = PiperVoice.load(model_path, config_path, use_cuda)
+
+        # Set download directory to first data directory by default
+        if not download_dir:
+            download_dir = os.path.join(os.path.expanduser('~'), '.piper', 'data')
+            logging.debug(f"Download directory not provided. Using default: {download_dir}")
+            try:
+                os.makedirs(download_dir, exist_ok=True)
+            except Exception as e:
+                logger.error(f"Error creating download directory: {e}")
+                raise
+
+        # Download voice if file doesn't exist
+        model_path = Path(model_path)
+        model_path_str = str(model_path)
+        if not model_path.exists():
+            # Load voice info
+            try:
+                voices_info = get_voices(download_dir, update_voices=True)
+            except Exception as e:
+                print(f"Error getting voices: {e}")  # Print any exceptions raised by get_voices
+                raise
+
+            try:
+
+                print(f"Model path: {model_path}")
+                print(f"Voices info keys: {voices_info.keys()}")
+                # Check if model_path is in voices_info
+                if model_path_str not in voices_info:
+                    print(f"Voice not found in voices_info: {model_path_str}")
+                else:
+                    print(f"Voice found in voices_info: {model_path_str}")
+
+                print(f"Ensuring voice exists: {model_path}, {download_dir}, {download_dir}")
+                ensure_voice_exists(model_path_str, [download_dir], download_dir, voices_info)
+                print(f"Voice exists: {model_path}")
+                print(f"Finding voice: {model_path}, {download_dir}")
+                model_path, config_path = find_voice(model_path, [download_dir])
+            except Exception as e:
+                logger.error(f"Error loading voice: {e}")
+                raise
+        print(f"Loading voice: {model_path}, {config_path}, {use_cuda}")    
+        self._client = PiperVoice.load(str(model_path), config_path, use_cuda)
 
     def synth(self, text: str, format: str, speaker_id: Optional[int] = None, length_scale: Optional[float] = None, noise_scale: Optional[float] = None, noise_w: Optional[float] = None, sentence_silence: float = 0.0) -> bytes:
         try:
