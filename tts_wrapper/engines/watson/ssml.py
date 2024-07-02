465082299240aaca801189ef7f1111850da06e4a
Chafid Ahmad <chafid@gmail.com>
2024-07-02 18:55:22 +0700

    fix issue with watson ssml

--- tts_wrapper/engines/watson/ssml.py
+++ tts_wrapper/engines/watson/ssml.py
@@ -1,4 +1,9 @@
 from tts_wrapper.ssml.ssml_node import SSMLNode
 from ...ssml import BaseSSMLRoot
 
-WatsonSSML = BaseSSMLRoot
+class WatsonSSML(BaseSSMLRoot):
+    def __init__(self) -> None:
+        super().__init__()
+        
+    def clear_ssml(self):
+        pass 
